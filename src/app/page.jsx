c6b2db1dad18f2a51bd2c433b23912715b40f5d7
Daniel Brownsey <danielbrownsey01@gmail.com>
2025-10-28 09:33:28 +0000

    add styles for homepage

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -8,9 +8,11 @@ export default async function HomePage() {
       <p className="h-screen bg-gradient-to-br from-green-800 via-green-600 to-green-300">
         Welcome to LeaseLink!
       </p>
-      <div className="home-page-signs">
-        <SignInButton />
-        <SignOutButton />
+      <div className="home-page-container">
+        <div className="home-page-signs">
+          <SignInButton />
+          <SignOutButton />
+        </div>
       </div>
     </div>
   );
